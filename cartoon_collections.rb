d4e498fded6181a8ab4e7339da090ebe52aaa967
Arya <arya721@gmail.com>
2019-09-11 22:23:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,20 +1,4 @@
-def roll_call_dwarves(dwarfs)
-  dwarfs.each_with_index { |name, index|
-    puts "#{index + 1}. #{name}" }
-end
 
-def summon_captain_planet(veggies)
-  veggies.map do |names|
-    "#{names.capitalize}!"
-  end
-end
-
-def long_planeteer_calls(calls)
-  calls.reduce() do |memo, calls|
-    if calls.length > 4
-      return true
-    end
-  end
 
 
   def find_the_cheese(contains_cheese)
