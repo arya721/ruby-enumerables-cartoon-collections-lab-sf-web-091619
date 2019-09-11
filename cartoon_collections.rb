0e0e07c4164e3a3fc0d9a1d2e7fe3af4f903d7d9
Arya <arya721@gmail.com>
2019-09-11 22:09:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,9 +4,10 @@ def roll_call_dwarves(dwarfs)
 end
 
 
-def summon_captain_planet(veggies,names)
-veggies.capitalize("names")
-return names
+def summon_captain_planet(veggies)
+  veggies.map do |names| 
+    "#{names.capitalize}!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
