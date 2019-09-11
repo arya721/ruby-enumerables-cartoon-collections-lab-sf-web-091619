2c4ae43847c76f90a8cf4820ae1940684e6342f2
Arya <arya721@gmail.com>
2019-09-11 22:08:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,12 @@
-def roll_call_dwarves(dwarfs) 
+def roll_call_dwarves(dwarfs)
   dwarfs.each_with_index { |name, index|
     puts "#{index + 1}. #{name}" }
 end
 
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies,names)
+veggies.capitalize("names")
+return names
 end
 
 def long_planeteer_calls# code an argument here
